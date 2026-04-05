ebabf3f50ebc905c219f1f493792e1d57bc2b193
Saloni Sharma <saloni94666@gmail.com>
2026-04-05 17:28:31 +0530

    new code

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -12,7 +12,7 @@ const ResetPassword = () => {
   const [newPassword, setNewPassword] = useState('');
   // const [isEmailSent, setIsEmailSent] = useState('');
   const [isEmailSent, setIsEmailSent] = useState(false);
-  const [otp, setOtp] = useState('');
+  const [otp, setOtp] = useState(0);
   const [isOtpSubmited, setIsOtpSubmited] = useState(false);
 
   const { backendUrl } = useContext(AppContext);
