be95a0270aeee844af3e667690a3ba4957102fe9
Saloni Sharma <saloni94666@gmail.com>
2026-04-07 14:07:22 +0530

    new code

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -47,7 +47,9 @@ const ResetPassword = () => {
   const onSubmitEmail = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await axios.post(`${backendUrl}/api/auth/send-reset-otp`, { email })
+      const { data } = await axios.post(`${backendUrl}/api/auth/send-reset-otp`, { email },{ withCredentials: true, timeout: 15000 });
+
+      console.log("OTP Response:", data);
       if (data.success) {
         toast.success(data.message);
         setIsEmailSent(true);
